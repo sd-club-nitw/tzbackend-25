a7f32b414d5157ac8591dd6a9679cf981847e4cb
sd-club-nitw <sd_club@nitw.ac.in>
2025-10-07 19:05:14 +0530

    changed api routes to /api

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGO_URI)
 .catch(err => console.log(err))
 
 const authRoutes = require('./routes/auth')
-app.use('/auth', authRoutes)
+app.use('/api/auth', authRoutes)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
